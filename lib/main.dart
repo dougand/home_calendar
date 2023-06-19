7f7805e1f2c6e43d74406616068972a353a2e847
dougand <doug@oakcottagestudios.com>
2023-06-19 16:35:59 +0100

    Initial calendar view

--- lib/main.dart
+++ lib/main.dart
@@ -2,16 +2,19 @@
 // SPDX-License-Identifier: Apache-2.0
 
 import 'package:flutter/material.dart';
-import 'package:intl/date_symbol_data_local.dart';
+//import 'package:intl/date_symbol_data_local.dart';
+import 'package:home_calendar/utils.dart';
+import 'package:table_calendar/table_calendar.dart';
 
-import 'pages/basics_example.dart';
-import 'pages/complex_example.dart';
-import 'pages/events_example.dart';
-import 'pages/multi_example.dart';
-import 'pages/range_example.dart';
+// import 'pages/basics_example.dart';
+// import 'pages/complex_example.dart';
+// import 'pages/events_example.dart';
+// import 'pages/multi_example.dart';
+// import 'pages/range_example.dart';
 
 void main() {
-  initializeDateFormatting().then((_) => runApp(MyApp()));
+  //initializeDateFormatting().then((_) => runApp(MyApp()));
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
@@ -22,71 +25,160 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: StartPage(),
+      home: CalendarPage(),
     );
   }
 }
 
-class StartPage extends StatefulWidget {
+
+class CalendarPage extends StatefulWidget {
   @override
-  _StartPageState createState() => _StartPageState();
+  _CalendarPage createState() => _CalendarPage();
 }
 
-class _StartPageState extends State<StartPage> {
+class _CalendarPage extends State<CalendarPage> {
+
+  CalendarFormat _calendarFormat = CalendarFormat.month;
+  RangeSelectionMode _rangeSelectionMode = RangeSelectionMode
+      .toggledOff; // Can be toggled on/off by longpressing a date
+  DateTime _focusedDay = DateTime.now();
+  DateTime? _selectedDay;
+  DateTime? _rangeStart;
+  DateTime? _rangeEnd;
+
+
+  void _onDaySelected(DateTime selectedDay, DateTime focusedDay) {
+    if (!isSameDay(_selectedDay, selectedDay)) {
+      setState(() {
+        _selectedDay = selectedDay;
+        _focusedDay = focusedDay;
+        _rangeStart = null; // Important to clean those
+        _rangeEnd = null;
+        _rangeSelectionMode = RangeSelectionMode.toggledOff;
+      });
+
+//      _selectedEvents.value = _getEventsForDay(selectedDay);
+    }
+  }
+
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('TableCalendar Example'),
+        title: const Text('Home Calendar'),
       ),
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            const SizedBox(height: 20.0),
-            ElevatedButton(
-              child: Text('Basics'),
-              onPressed: () => Navigator.push(
-                context,
-                MaterialPageRoute(builder: (_) => TableBasicsExample()),
+
+            TableCalendar<Event>(
+              firstDay: kFirstDay,
+              lastDay: kLastDay,
+              focusedDay: _focusedDay,
+              selectedDayPredicate: (day) => isSameDay(_selectedDay, day),
+              rangeStartDay: _rangeStart,
+              rangeEndDay: _rangeEnd,
+              calendarFormat: _calendarFormat,
+              rangeSelectionMode: _rangeSelectionMode,
+              //eventLoader: _getEventsForDay,
+              startingDayOfWeek: StartingDayOfWeek.monday,
+              calendarStyle: CalendarStyle(
+                // Use `CalendarStyle` to customize the UI
+                outsideDaysVisible: false,
               ),
-            ),
-            const SizedBox(height: 12.0),
-            ElevatedButton(
-              child: Text('Range Selection'),
-              onPressed: () => Navigator.push(
-                context,
-                MaterialPageRoute(builder: (_) => TableRangeExample()),
+              headerStyle: HeaderStyle(
+                formatButtonVisible: false,
               ),
+              onDaySelected: _onDaySelected,
+              //onRangeSelected: _onRangeSelected,
+              onFormatChanged: (format) {
+                if (_calendarFormat != format) {
+                  setState(() {
+                    _calendarFormat = format;
+                  });
+                }
+              },
+              onPageChanged: (focusedDay) {
+                _focusedDay = focusedDay;
+              },
             ),
-            const SizedBox(height: 12.0),
-            ElevatedButton(
-              child: Text('Events'),
-              onPressed: () => Navigator.push(
-                context,
-                MaterialPageRoute(builder: (_) => TableEventsExample()),
-              ),
-            ),
-            const SizedBox(height: 12.0),
-            ElevatedButton(
-              child: Text('Multiple Selection'),
-              onPressed: () => Navigator.push(
-                context,
-                MaterialPageRoute(builder: (_) => TableMultiExample()),
-              ),
-            ),
-            const SizedBox(height: 12.0),
-            ElevatedButton(
-              child: Text('Complex'),
-              onPressed: () => Navigator.push(
-                context,
-                MaterialPageRoute(builder: (_) => TableComplexExample()),
-              ),
-            ),
-            const SizedBox(height: 20.0),
+
+            Spacer(),
           ],
         ),
       ),
     );
   }
 }
+
+
+//=================================================================
+//
+//    OLD menu
+//
+
+// class StartPage extends StatefulWidget {
+//   @override
+//   _StartPageState createState() => _StartPageState();
+// }
+//
+// class _StartPageState extends State<StartPage> {
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       appBar: AppBar(
+//         title: Text('TableCalendar Example'),
+//       ),
+//       body: Center(
+//         child: Column(
+//           mainAxisAlignment: MainAxisAlignment.center,
+//           children: [
+//             const SizedBox(height: 20.0),
+//             ElevatedButton(
+//               child: Text('Basics'),
+//               onPressed: () => Navigator.push(
+//                 context,
+//                 MaterialPageRoute(builder: (_) => TableBasicsExample()),
+//               ),
+//             ),
+//             const SizedBox(height: 12.0),
+//             ElevatedButton(
+//               child: Text('Range Selection'),
+//               onPressed: () => Navigator.push(
+//                 context,
+//                 MaterialPageRoute(builder: (_) => TableRangeExample()),
+//               ),
+//             ),
+//             const SizedBox(height: 12.0),
+//             ElevatedButton(
+//               child: Text('Events'),
+//               onPressed: () => Navigator.push(
+//                 context,
+//                 MaterialPageRoute(builder: (_) => TableEventsExample()),
+//               ),
+//             ),
+//             const SizedBox(height: 12.0),
+//             ElevatedButton(
+//               child: Text('Multiple Selection'),
+//               onPressed: () => Navigator.push(
+//                 context,
+//                 MaterialPageRoute(builder: (_) => TableMultiExample()),
+//               ),
+//             ),
+//             const SizedBox(height: 12.0),
+//             ElevatedButton(
+//               child: Text('Complex'),
+//               onPressed: () => Navigator.push(
+//                 context,
+//                 MaterialPageRoute(builder: (_) => TableComplexExample()),
+//               ),
+//             ),
+//             const SizedBox(height: 20.0),
+//           ],
+//         ),
+//       ),
+//     );
+//   }
+// }
