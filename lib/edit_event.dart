230d5295830fe81fc535c5a15f46d3facf0f8593
Doug <doug@oakcottagestudios.com>
2024-05-06 23:13:36 +0100

    Jellyfish update

--- lib/edit_event.dart
+++ lib/edit_event.dart
@@ -11,7 +11,11 @@
 //
 
 
+
+
 import 'package:flutter/material.dart';
+
+import 'event_list.dart';
 
 
 // Create a Form widget.
@@ -43,49 +47,72 @@ class EditEventFormState extends State<EditEventForm> {
   Widget build(BuildContext context) {
     // Build a Form widget using the _formKey created above.
     return Form(
+
       key: _formKey,
       child: Material(
 
 
 
         child: ListView(
-          padding: EdgeInsets.all(16),
+          padding: const EdgeInsets.all(16),
           children: [
-            editTitle(),
-            const SizedBox(height: 16),
-            editDate(),
-            const SizedBox(height: 16),
-            editDescription(),
-            const SizedBox(height: 16),
-            editColour(),
-            const SizedBox(height: 32),
 
-            // TextFormField(
-            //   // The validator receives the text that the user has entered.
-            //   validator: (value) {
-            //     if (value == null || value.isEmpty) {
-            //       return 'Please enter some text';
-            //     }
-            //     return null;
-            //   },
+            // ListTile(
+            //     title: new TextField(
+            //       decoration: new InputDecoration(
+            //         hintText: "New Entry",
+            //       ),
+            //   ),
             // ),
-            Padding(
-              padding: const EdgeInsets.symmetric(vertical: 16),
-              child: ElevatedButton(
-                onPressed: () {
-                  // Validate returns true if the form is valid, or false otherwise.
-                  if (_formKey.currentState!.validate()) {
-                    // If the form is valid, display a snackbar. In the real world,
-                    // you'd often call a server or save the information in a database.
-                    ScaffoldMessenger.of(context).showSnackBar(
-                      const SnackBar(content: Text('Processing Data')),
-                    );
-                  }
-                },
-                child: const Text('Submit'),
-              ),
-            ),
-          ],
+
+            // ListView(
+            //     padding: EdgeInsets.all(16),
+            //   children: [
+                editTitle(),
+                const SizedBox(height: 16),
+                editDate(),
+                const SizedBox(height: 16),
+                editDescription(),
+                const SizedBox(height: 16),
+                editColour(),
+                const SizedBox(height: 32),
+
+                // TextFormField(
+                //   // The validator receives the text that the user has entered.
+                //   validator: (value) {
+                //     if (value == null || value.isEmpty) {
+                //       return 'Please enter some text';
+                //     }
+                //     return null;
+                //   },
+                // ),
+                Padding(
+                  padding: const EdgeInsets.symmetric(vertical: 16),
+                  child: ElevatedButton(
+                    onPressed: () {
+
+                      print('Submit pressed');
+                        ScaffoldMessenger.of(context).showSnackBar(
+                          const SnackBar(content: Text('Saving Data')),
+                        );
+
+                        EventList().saveEventsToFile();
+
+                      // Validate returns true if the form is valid, or false otherwise.
+                      // if (_formKey.currentState!.validate()) {
+                      //   // If the form is valid, display a snackbar. In the real world,
+                      //   // you'd often call a server or save the information in a database.
+                      //   ScaffoldMessenger.of(context).showSnackBar(
+                      //     const SnackBar(content: Text('Processing Data')),
+                      //   );
+                      //}
+                    },
+                    child: const Text('Submit'),
+                  ),
+                ),
+              ],
+            // ),
+         // ],
         ),
       ),
     );
@@ -93,7 +120,7 @@ class EditEventFormState extends State<EditEventForm> {
 
 
   Widget editTitle() => TextFormField(
-    decoration: InputDecoration(
+    decoration: const InputDecoration(
       labelText: 'Title',
       border: OutlineInputBorder(),
     ),
@@ -102,7 +129,7 @@ class EditEventFormState extends State<EditEventForm> {
 
 
   Widget editDate() => TextFormField(
-    decoration: InputDecoration(
+    decoration: const InputDecoration(
       labelText: 'Date/Time',
       border: OutlineInputBorder(),
     ),
@@ -111,7 +138,7 @@ class EditEventFormState extends State<EditEventForm> {
 
 
   Widget editDescription() => TextFormField(
-    decoration: InputDecoration(
+    decoration: const InputDecoration(
       labelText: 'Description',
       border: OutlineInputBorder(),
     ),
@@ -120,7 +147,7 @@ class EditEventFormState extends State<EditEventForm> {
 
 
   Widget editColour() => TextFormField(
-    decoration: InputDecoration(
+    decoration: const InputDecoration(
       labelText: 'Colour',
       border: OutlineInputBorder(),
     ),
--- lib/event_list.dart
+++ lib/event_list.dart
@@ -1,7 +1,14 @@
 
-import 'dart:ui';
+//import 'dart:html';
+//import 'dart:ui';
+import 'dart:async';
+import 'dart:io';
 
 import 'package:flutter/material.dart';
+import 'package:path_provider/path_provider.dart';
+
+
+
 
 class Event {
 
@@ -16,7 +23,65 @@ class Event {
 
 class EventList {
 
+  static final EventList _instance = EventList._internal();
+
+  // using a factory is important
+  // because it promises to return _an_ object of this type
+  // but it doesn't promise to make a new one.
+  factory EventList() {
+  return _instance;
+  }
+
+  // This named constructor is the "real" constructor
+  // It'll be called exactly once, by the static property assignment above
+  // it's also private, so it can only be called in this class
+  EventList._internal() {
+  // initialization logic
+    print('EventList initialised');
+    loadEventsFromFile();
+  }
+
+
+
   List<Event>? events;
+
+
+  Future<File> saveEventsToFile() async {
+    final file = await _localFile;
+
+    // Write the file
+    return file.writeAsString('This is a test');
+  }
+
+
+  Future<int> loadEventsFromFile() async {
+    try {
+      final file = await _localFile;
+
+      // Read the file
+      final contents = await file.readAsString();
+
+      print('loadEventsFromFile() $contents');
+
+      return 1;
+    } catch (e) {
+      // If encountering an error, return 0
+      return 0;
+    }
+  }
+
+
+  Future<File> get _localFile async {
+    final path = await _localPath;
+    return File('$path/counter.txt');
+  }
+
+
+  Future<String> get _localPath async {
+    final directory = await getApplicationDocumentsDirectory();
+
+    return directory.path;
+  }
 
 }
 
